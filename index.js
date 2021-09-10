c94b85a6c1fd357ec1a2200dba41bd00b1b33860
Jass1321 <jassyra13216@gmail.com>
2021-09-10 11:13:13 -0500

    lab04

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ var express = require('express');
 var app = express();
 
 app.get('/',c_inicio);
-app.get('/saludo',c_saludo);
+app.get('/clientes',c_cliente);
+app.get('/productos',c_producto);
 
 function c_inicio(req,res) {
-    res.send('<h1>Hola Express</h1>');
+    res.send('<h1>Hola Express, soy Jassyra Espinoza</h1>');
 }
-function c_saludo(req,res) {
-    res.send('<h1>Pagina de saludo Express</h1>');
+function c_cliente(req,res) {
+    res.send('<h1>CLIENTES</h1><ul><li>Kiara Pinedo</li><li>Sandra Alcantara</li><li>Danna Vila</li></ul>');
+}
+function c_producto(req,res) {
+    res.send('<h1>PRODUCTOS</h1><ul><li>Gorras</li><li>Polos</li><li>Zapatillas</li></ul>');
 }
 
 function c_server() {
@@ -17,4 +21,4 @@ function c_server() {
     console.log(host + ':' + port);
 }
 
-var server = app.listen(8000,c_server);
+var server = app.listen(5000,c_server);
